a449fa2edcc8480a447b71498380697ced6cc071
fuainaru <muhammadghifarshop@gmail.com>
2023-03-15 01:50:52 +0700

    feat: add more tags

--- src/components/AnimeCard/AnimeCardTitle.tsx
+++ src/components/AnimeCard/AnimeCardTitle.tsx
@@ -4,19 +4,25 @@ import { useAnimeCardContext } from "./AnimeCardContext";
 
 const AnimeCardTitle = () => {
   const { anime } = useAnimeCardContext();
-  const splitTags = anime.tags.split(" ");
-  const sliceTags = splitTags.slice(0, 3);
+  const tags = anime.tags.split(" ");
+  const tagsSplit = tags.slice(0, 3);
 
   return (
     <div className="flex items-center gap-2 overflow-hidden my-3">
-      {sliceTags.map((tag, id) => (
+      {tagsSplit.map((tag, id) => (
         <div
           key={id}
           className="py-1 px-3 bg-[#383840] text-white rounded-full"
         >
-          <p className="text-xs">{tag}</p>
+          <p className="text-xs break-all truncate">{tag}</p>
         </div>
       ))}
+
+      {tags.length > 3 ? (
+        <div className="py-1 px-3 bg-[#383840] text-white rounded-full">
+          <p className="text-xs break-all truncate">+{tags.length - 3}</p>
+        </div>
+      ) : null}
     </div>
   );
 };
