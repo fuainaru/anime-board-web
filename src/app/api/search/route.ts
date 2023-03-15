e3061945e5ec8637ec5f90ac50c994356027cdb9
fuainaru <muhammadghifarshop@gmail.com>
2023-03-15 08:57:37 +0700

    feat: add button scroll tags

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -4,7 +4,9 @@ export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const tags = searchParams.get("tags");
 
-  const response = await fetch(API_URL + "/post.json?tags=" + tags);
+  const response = await fetch(
+    API_URL + "/post.json?tags=" + tags + "+rating:safe"
+  );
   const data = await response.json();
 
   return new Response(JSON.stringify(data), {
--- src/components/AnimeCard/AnimeCardTitle.tsx
+++ src/components/AnimeCard/AnimeCardTitle.tsx
@@ -1,28 +1,59 @@
 "use client";
 
+import { useCallback, useMemo, useRef } from "react";
+// @ts-ignore
+import IconLeft from "@iconscout/react-unicons/icons/uil-angle-left";
+// @ts-ignore
+import IconRight from "@iconscout/react-unicons/icons/uil-angle-right";
+
 import { useAnimeCardContext } from "./AnimeCardContext";
+
+const SCROLL_NUMBER = 100;
 
 const AnimeCardTitle = () => {
   const { anime } = useAnimeCardContext();
-  const tags = anime.tags.split(" ");
-  const tagsSplit = tags.slice(0, 3);
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  const tags = useMemo(() => {
+    return anime.tags.split(" ");
+  }, [anime.tags]);
+
+  const onScrollLeft = useCallback(() => {
+    if (ref.current) {
+      ref.current.scrollLeft += -SCROLL_NUMBER;
+    }
+  }, []);
+
+  const onScrollRight = useCallback(() => {
+    if (ref.current) {
+      ref.current.scrollLeft += +SCROLL_NUMBER;
+    }
+  }, []);
 
   return (
-    <div className="flex items-center gap-2 overflow-hidden my-3">
-      {tagsSplit.map((tag, id) => (
-        <div
-          key={id}
-          className="py-1 px-3 bg-[#383840] text-white rounded-full"
-        >
-          <p className="text-xs break-all truncate">{tag}</p>
-        </div>
-      ))}
+    <div className="flex items-center justify-between my-3">
+      <IconLeft onClick={onScrollLeft} color="#797981" />
 
-      {tags.length > 3 ? (
-        <div className="py-1 px-3 bg-[#383840] text-white rounded-full">
-          <p className="text-xs break-all truncate">+{tags.length - 3}</p>
-        </div>
-      ) : null}
+      <div
+        ref={ref}
+        className="flex items-center gap-2 overflow-x-scroll scroll-smooth no-scrollbar"
+      >
+        {tags.map((tag, id) => (
+          <div
+            key={id}
+            className="py-1 px-3 bg-[#383840] text-white rounded-full"
+          >
+            <p className="text-xs break-all truncate">{tag}</p>
+          </div>
+        ))}
+      </div>
+
+      <IconRight
+        onClick={onScrollRight}
+        color="#797981"
+        className="!w-36 !h-5"
+      />
     </div>
   );
 };
